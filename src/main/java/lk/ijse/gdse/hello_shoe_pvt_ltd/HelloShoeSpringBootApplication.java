56498b32aeeac43f10181e9773bdc6e71f3388ca
Maheesha <maheeshafernando18@gmail.com>
2024-04-22 23:45:08 +0530

    init commit

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/HelloShoeSpringBootApplication.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class HelloShoeSpringBootApplication {
+    @Bean
+    ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloShoeSpringBootApplication.class, args);
+    }
+
+}
