d731b7e7350198b8eac162d95ba595c1e455b5db
Maheesha <maheeshafernando18@gmail.com>
2024-04-22 23:48:30 +0530

    add supplier entity

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/SupplierEntity.java
@@ -0,0 +1,30 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "supplier")
+public class SupplierEntity implements SuperEntity{
+    @Id
+    private String supplier_code;
+    private String supplier_name;
+    private Category category;
+    private String building_no;
+    private String lane;
+    private String city;
+    private String state;
+    private String postal_code;
+    private String country;
+    private String contact_01;
+    private String contact_02;
+    private String email;
+}
