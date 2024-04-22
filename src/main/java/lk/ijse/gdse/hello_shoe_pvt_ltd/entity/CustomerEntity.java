54632bb42767bcbab533c02bd7833b833d320727
Maheesha <maheeshafernando18@gmail.com>
2024-04-22 23:48:09 +0530

    add customer entity

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/CustomerEntity.java
@@ -0,0 +1,39 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Gender;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Level;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "customer")
+public class CustomerEntity implements SuperEntity {
+    @Id
+    private String customer_code;
+    private String customer_name;
+    private Gender gender;
+    private Date joined_date;
+    private Level level;
+    private int total_points;
+    private Date dob;
+    private String building_no;
+    private String lane;
+    private String city;
+    private String state;
+    private String postal_code;
+    private String contact;
+    private String email;
+    private Timestamp recent_purchase;
+
+
+}
