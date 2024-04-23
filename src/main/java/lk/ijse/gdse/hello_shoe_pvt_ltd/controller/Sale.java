29d2ba5582e322ff5fc592e0d23a636b50ea6a1e
Maheesha <maheeshafernando18@gmail.com>
2024-04-23 20:25:43 +0530

    check health in sale controller

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Sale.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Sale.java
@@ -1,4 +1,16 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/sale")
+@RequiredArgsConstructor
 public class Sale {
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "Hello I'm sale controller and I'm okay!";
+    }
 }
