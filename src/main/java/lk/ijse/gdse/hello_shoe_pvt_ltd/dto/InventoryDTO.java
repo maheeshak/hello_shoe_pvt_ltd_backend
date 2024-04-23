530286db966ad8f0b39d141f62001a2bffd9cfad
Maheesha <maheeshafernando18@gmail.com>
2024-04-23 12:12:21 +0530

    create Inventory DTO

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/dto/InventoryDTO.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/dto/InventoryDTO.java
@@ -8,5 +8,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class InventoryDTO implements SuperDTO{
+    private String item_code;
+    private String item_desc;
+    private String item_pic;
+    private int qty_on_hand;
+    private String category;
+    private int size;
+    private String supplier_code;
+    private String supplier_name;
+    private double selling_price;
+    private double buying_price;
+    private double expected_profit;
+    private double profit_margin;
+    private String status;
+
 
 }
