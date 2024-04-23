48abf68c18dddd2e64dd16a5e2be1315b5030519
Maheesha <maheeshafernando18@gmail.com>
2024-04-23 14:36:58 +0530

    implement return Entity

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/ReturnEntity.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/ReturnEntity.java
@@ -1,4 +1,23 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "return")
 public class ReturnEntity {
+    @Id
+    private String return_id;
+    private String order_id;
+    private Date return_date;
+    private String return_reason;
 }
