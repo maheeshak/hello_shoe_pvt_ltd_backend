e5d39da506d8139d5534c384e08436c59ecdff61
Maheesha <maheeshafernando18@gmail.com>
2024-04-25 09:28:18 +0530

    implement size entity

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/SizeEntity.java
@@ -0,0 +1,21 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "size")
+public class SizeEntity implements SuperEntity {
+    @Id
+    private String size_id;
+    private int size;
+    private String category;
+
+}
