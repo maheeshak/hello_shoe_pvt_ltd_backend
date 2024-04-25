e7001786d9def37be39c161980a51b835b5c1b6d
Maheesha <maheeshafernando18@gmail.com>
2024-04-25 10:18:04 +0530

    completed controller class

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Size.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Size.java
@@ -1,17 +1,49 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.controller;
 
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SaleDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SizeDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.service.SizeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/size")
 @RequiredArgsConstructor
 public class Size {
+    private final SizeService sizeService;
 
     @GetMapping("/health")
     public String healthCheck(){
         return "Hello I'm size controller and I'm okay!";
+    }
+
+    @PostMapping
+    public boolean saveSize(@RequestBody SizeDTO sizeDTO){
+        return sizeService.saveSize(sizeDTO);
+    }
+
+    @DeleteMapping
+    public boolean deleteSize(@RequestParam String size_id){
+        return sizeService.deleteSize(size_id);
+    }
+
+    @PutMapping
+    public boolean updateSize(@RequestBody SizeDTO sizeDTO){
+        return sizeService.updateSize(sizeDTO);
+    }
+
+    @GetMapping("/find")
+    public SizeDTO searchSize(@RequestParam String size_id){
+        System.out.println(size_id);
+        SizeDTO sizeDTO = new SizeDTO();
+        sizeDTO.setSize_id(size_id);
+        return sizeDTO;
+    }
+
+    @GetMapping
+    public SizeDTO getAllSizes(){
+        SizeDTO sizeDTO = new SizeDTO();
+        sizeDTO.setSize_id("S001");
+        return sizeDTO;
     }
 }
--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/impl/SizeServiceImpl.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/impl/SizeServiceImpl.java
@@ -1,8 +1,12 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.service.impl;
 
 import jakarta.transaction.Transactional;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dao.ReturnRepo;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dao.SaleRepo;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SizeDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.service.SizeService;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Converter;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Mapping;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +16,9 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 public class SizeServiceImpl implements SizeService {
+    private final SaleRepo saleRepo;
+    private final Mapping mapping;
+    private final Converter converter;
     @Override
     public boolean saveSize(SizeDTO sizeDTO) {
         return false;
