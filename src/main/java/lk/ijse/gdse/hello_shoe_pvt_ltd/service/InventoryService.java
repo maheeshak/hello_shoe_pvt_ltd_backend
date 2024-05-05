393fd1fed0ec3d83be3d90ecc519ba3649025f83
Maheesha <maheeshafernando18@gmail.com>
2024-05-05 14:57:43 +0530

    change inventoryService interface

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/InventoryService.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/InventoryService.java
@@ -1,11 +1,13 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
 
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.InventoryDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.extra.InventoryDetailsDTO;
 
 import java.util.List;
 
 public interface InventoryService {
     public boolean saveInventory(InventoryDTO inventoryDTO);
+    public boolean add(InventoryDetailsDTO inventoryDetailsDTO);
     public boolean deleteInventory(String id);
     public boolean updateInventory(InventoryDTO inventoryDTO);
     public List<InventoryDTO> getAllInventories();
