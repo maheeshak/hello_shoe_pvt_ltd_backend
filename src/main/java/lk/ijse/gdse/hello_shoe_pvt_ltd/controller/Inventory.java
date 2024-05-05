b139b675f90041842ee9dc987ff26cdeb7cdfa21
Maheesha <maheeshafernando18@gmail.com>
2024-05-05 14:50:52 +0530

    create Inventory controller

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Inventory.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Inventory.java
@@ -2,6 +2,7 @@ package lk.ijse.gdse.hello_shoe_pvt_ltd.controller;
 
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.CustomerDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.InventoryDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.extra.InventoryDetailsDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.service.InventoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +17,10 @@ public class Inventory {
 
         return "Hello I'm inventory controller and I'm okay!";
     }
-    @PostMapping
-    public boolean saveInventory(@RequestBody InventoryDTO inventoryDTO){
-        return inventoryService.saveInventory(inventoryDTO);
 
+    @PostMapping
+    public boolean saveInventory(@RequestBody InventoryDetailsDTO inventoryDetailsDTO) {
+        return inventoryService.add(inventoryDetailsDTO);
     }
 
     @PutMapping
