de3817ec1d8e1bcac4e16e7d17bc75f8a56108ae
Maheesha <maheeshafernando18@gmail.com>
2024-05-05 14:59:02 +0530

    change InventoryServiceImpl

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/impl/InventoryServiceImpl.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/impl/InventoryServiceImpl.java
@@ -3,8 +3,11 @@ package lk.ijse.gdse.hello_shoe_pvt_ltd.service.impl;
 import jakarta.transaction.Transactional;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dao.EmployeeRepo;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dao.InventoryRepo;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dao.SizeInventoryDetailsRepo;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.InventoryDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.extra.InventoryDetailsDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.InventoryEntity;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.SizeInventoryDetailsEntity;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.service.InventoryService;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Converter;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Mapping;
@@ -21,13 +24,34 @@ public class InventoryServiceImpl implements InventoryService {
     private final InventoryRepo inventoryRepo;
     private final Mapping mapping;
     private final Converter converter;
+
+    private final SizeInventoryDetailsRepo sizeInventoryDetailsRepo;
+
     @Override
     public boolean saveInventory(InventoryDTO inventoryDTO) {
-        InventoryEntity inventoryEntity = mapping.toInventoryEntity(inventoryDTO);
-        inventoryRepo.save(inventoryEntity);
-        return true;
+        return false;
     }
 
+    public boolean add(InventoryDetailsDTO inventoryDetailsDTO) {
+
+        if (inventoryRepo.existsById(inventoryDetailsDTO.getInventoryDTO().getItem_code())) {
+            return false;
+        }
+
+        InventoryEntity inventoryEntity = mapping.toInventoryEntity(inventoryDetailsDTO.getInventoryDTO());
+        List<SizeInventoryDetailsEntity> sizeInventoryDetailsEntities = mapping.mapToSizeInventoryDetailsEntity(inventoryDetailsDTO.getSizeInventoryDetailsDTO());
+
+        inventoryRepo.save(inventoryEntity);
+        sizeInventoryDetailsRepo.saveAll(sizeInventoryDetailsEntities);
+
+        if (inventoryRepo.existsById(inventoryEntity.getItem_code())) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
     @Override
     public boolean deleteInventory(String id) {
         Optional<InventoryEntity> tmpInventory = inventoryRepo.findById(id);
