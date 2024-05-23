45177edd1733a137674e5a0d9f4539763f204989
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 19:05:26 +0530

    complete user service

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/UserService.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/UserService.java
@@ -1,9 +1,15 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
 
+
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.UserDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService {
     UserDetailsService userDetailsService();
-    void save(UserDTO userDTO);
+    void saveUser(UserDTO userDTO);
+
+    String getRole(String email);
+
+    String getEmployeeCode(String cashierName);
 }
