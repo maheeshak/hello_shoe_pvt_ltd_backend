7c23c4b675d66681c84c090f4f7116458fa8ff94
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 19:05:42 +0530

    complete customer service impl

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/impl/CustomerServiceIMPL.java
@@ -0,0 +1,119 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.service.impl;
+
+import jakarta.transaction.Transactional;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.repository.CustomerRepo;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.CustomerDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.CustomerEntity;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.service.CustomerService;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.convert.Converter;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.map.Mapping;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CustomerServiceIMPL implements CustomerService {
+
+    private final CustomerRepo customerRepo;
+    private final Mapping mapping;
+    private final Converter converter;
+
+
+    @Override
+    public boolean add(CustomerDTO customerDTO) {
+        CustomerEntity customerEntity = mapping.mapToCustomerEntity(customerDTO);
+        customerEntity.setActive_state("ACTIVE");
+        customerRepo.save(customerEntity);
+        return customerRepo.existsById(customerEntity.getCustomer_code());
+    }
+
+    @Override
+    public boolean delete(String customer_code) {
+        if (customerRepo.existsById(customer_code)) {
+
+            customerRepo.changeActiveState("DEACTIVATE", customer_code);
+
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
+    @Override
+    public boolean update(CustomerDTO customerDTO) {
+       /* Optional<CustomerEntity> tmpCustomer = customerRepo.findById(customerDTO.getCustomer_code());
+        if (tmpCustomer.isPresent()) {
+            converter.convertCustomerEntity(customerDTO, tmpCustomer.get());
+            return true;
+        }
+        return false;*/
+        customerDTO.setActive_state("ACTIVE");
+        return add(customerDTO);
+    }
+
+    @Override
+    public CustomerDTO search(String customer_code) {
+        CustomerEntity referenceCustomerEntity = customerRepo.getReferenceById(customer_code);
+        return mapping.mapToCustomerDTO(referenceCustomerEntity);
+    }
+
+    @Override
+    public List<CustomerDTO> getAll() {
+        return mapping.mapToCustomerDTOList(customerRepo.findAll());
+    }
+
+    @Override
+    public String generateCustomerID() {
+        String customer_code = customerRepo.generateCustomerID();
+        if (customer_code == null) {
+            return "C001";
+        } else {
+            int customerID = Integer.parseInt(customer_code.replace("C", ""));
+            customerID++;
+            if (customerID < 10) {
+                return "C00" + customerID;
+            } else if (customerID < 100) {
+                return "C0" + customerID;
+            } else {
+                return "C" + customerID;
+            }
+        }
+    }
+
+    @Override
+    public CustomerDTO searchContact(String customerContact) {
+
+        CustomerEntity customerEntity = customerRepo.searchContact(customerContact);
+        if (customerEntity != null) {
+            return mapping.mapToCustomerDTO(customerEntity);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void updatePoints(String customerCode, double addedPoints) {
+        CustomerEntity customerEntity = customerRepo.getReferenceById(customerCode);
+        Integer currentPoints = customerEntity.getPoint();
+        if (currentPoints == 0 ) {
+            currentPoints = 0;
+        }
+        double newPoints = currentPoints + addedPoints;
+        customerRepo.updatePoints(customerCode, newPoints);
+        customerRepo.updateLastPurchaseDate(customerCode,   new Timestamp(new Date().getTime()));
+
+    }
+
+    @Override
+    public String getCustomerCount() {
+        return customerRepo.getCustomerCount();
+    }
+}
