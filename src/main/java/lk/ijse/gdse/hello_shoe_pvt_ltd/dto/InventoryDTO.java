df46f3b57d50553c87112a891aab9fcaf28cab03
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 19:07:17 +0530

    add inventory dto

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/dto/InventoryDTO.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/dto/InventoryDTO.java
@@ -1,25 +1,22 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.dto;
 
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Gender;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Occasion;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Verities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class InventoryDTO implements SuperDTO{
     private String item_code;
     private String item_desc;
     private String item_pic;
-    private String gender;
-    private String occasion;
-    private String variety;
-
-    @ManyToOne
-    @JoinColumn(name = "supplier_code", referencedColumnName = "supplier_code")
-    private SupplierDTO supplierDTO;
-
-
+    private Occasion occasion;
+    private Gender gender;
+    private Verities verities;
+    private SupplierDTO supplier;
 }
