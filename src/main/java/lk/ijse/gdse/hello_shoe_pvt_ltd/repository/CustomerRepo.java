4e9fca3fcd020ced609798bd0a447b5e8f9fa9f1
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 19:01:05 +0530

    complete customer repo

---
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/repository/CustomerRepo.java
@@ -0,0 +1,39 @@
+package lk.ijse.gdse.hello_shoe_pvt_ltd.repository;
+
+import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Repository
+public interface CustomerRepo extends JpaRepository<CustomerEntity, String>{
+    @Query(value = "SELECT customer_code FROM customer ORDER BY customer_code DESC LIMIT 1", nativeQuery = true)
+    String generateCustomerID();
+
+    @Modifying
+    @Query(value = "UPDATE customer SET active_state = ?1 WHERE customer_code = ?2", nativeQuery = true)
+    void changeActiveState(String deactivate, String customerCode);
+
+    @Override
+    @Query(value = "SELECT * FROM customer WHERE active_state = 'ACTIVE'", nativeQuery = true)
+    List<CustomerEntity> findAll();
+
+    @Query(value = "SELECT * FROM customer WHERE contact = ?1", nativeQuery = true)
+    CustomerEntity searchContact(String customerContact);
+
+    @Modifying
+    @Query(value = "UPDATE customer SET point = ?2 WHERE customer_code = ?1", nativeQuery = true)
+    void updatePoints(String customerCode, double newPoints);
+
+    @Modifying
+    @Query(value = "UPDATE customer SET recent_purchase = ?2 WHERE customer_code = ?1", nativeQuery = true)
+    void updateLastPurchaseDate(String customerCode, Timestamp purchaseDate);
+
+    @Query(value = "SELECT COUNT(customer_code) FROM customer WHERE active_state = 'ACTIVE'", nativeQuery = true)
+    String getCustomerCount();
+
+}
