728d3c27176806943a57ee00fd4fc82ba57b6ac4
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 18:59:50 +0530

    complete employee entity

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/EmployeeEntity.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/EmployeeEntity.java
@@ -1,31 +1,35 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Gender;
-import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Role;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Designation;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Gender;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.sql.Date;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @Entity
 @Table(name = "employee")
-public class EmployeeEntity implements SuperEntity{
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class EmployeeEntity {
     @Id
     private String employee_code;
-    private String employee_name;
+    private String name;
+    @Lob
+    @Column(name = "profile_pic", columnDefinition = "LONGTEXT")
     private String profile_pic;
     private Gender gender;
     private String status;
-    private String Designation;
+    private Designation designation;
     private Role role;
     private Date dob;
     private Date joined_date;
-    private String building_no;
+    private String building_number;
     private String lane;
     private String city;
     private String state;
@@ -33,9 +37,12 @@ public class EmployeeEntity implements SuperEntity{
     private String contact;
     private String email;
     private String guardian_name;
-    private String emergency_contact;
+    private String guardian_contact;
+    @Column(name = "active_state",columnDefinition = "varchar(10) default 'ACTIVE'")
+    private String active_state;
 
     @ManyToOne
-    @JoinColumn(name = "branch_code", referencedColumnName = "branch_code")
+    @JoinColumn(name = "branch_code",referencedColumnName = "branch_code")
+    @JsonBackReference
     private BranchEntity branch;
 }
