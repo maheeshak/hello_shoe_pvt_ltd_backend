c334bf83236f55d86d4733a82ba4195bc3598249
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 18:35:02 +0530

    complete branch controller

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Branch.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Branch.java
@@ -1,49 +1,65 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.controller;
 
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.BranchDTO;
-
 import lk.ijse.gdse.hello_shoe_pvt_ltd.service.BranchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/branch")
 @RequiredArgsConstructor
-public class Branch {
+@CrossOrigin(origins = "*")
+public class Branch{
+
     private final BranchService branchService;
-    @GetMapping("/health")
-    public String healthCheck(){
-        return "Hello I'm branch controller and I'm okay!";
-    }
+
+
 
     @PostMapping
-    public boolean saveBranch(@RequestBody BranchDTO branchDTO){
-        return branchService.saveBranch(branchDTO);
-
+    public boolean saveBranch(@RequestBody BranchDTO branchDTO) {
+        return branchService.add(branchDTO);
     }
 
 
     @PutMapping
-    public boolean updateBranch(@RequestBody BranchDTO branchDTO){
-        return branchService.updateBranch(branchDTO);
+    public boolean updateBranch(@RequestBody BranchDTO branchDTO) {
+        return branchService.update(branchDTO);
     }
+
 
     @DeleteMapping
-    public boolean deleteBranch(@RequestParam String branch_code){
-        return branchService.deleteBranch(branch_code);
+    public boolean deleteBranch(@RequestParam String branch_code) {
+        return branchService.delete(branch_code);
     }
 
-    @GetMapping("/find")
-    public BranchDTO searchBranch(@RequestParam String branch_code){
-        return branchService.getSelectedBranch(branch_code);
-
-    }
 
     @GetMapping
-    public List<BranchDTO> getAllBranch(){
+    public BranchDTO searchBranch(@RequestParam String branch_code) {
+        try {
 
-       return branchService.getAllBranches();
+            return branchService.search(branch_code);
+        } catch (Exception e) {
+            return null;
+        }
+
     }
 
+
+    @GetMapping("/all")
+    public List<BranchDTO> getAllBranches() {
+        return branchService.getAll();
+    }
+
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Hello I'm Branch Controller. I'm OK! Have a nice day!";
+    }
+
+    @GetMapping("/id")
+    public String getBranchId() {
+        return branchService.getBranchCode();
+    }
 }
