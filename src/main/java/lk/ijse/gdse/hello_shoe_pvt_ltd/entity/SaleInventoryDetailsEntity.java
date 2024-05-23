2d6d9b14eec1cc4e6b84ec2226028863ec14ef7b
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 19:00:19 +0530

    complete sale inventory details entity

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/SaleInventoryDetailsEntity.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/SaleInventoryDetailsEntity.java
@@ -5,10 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+import java.util.List;
+
 @Entity
-@Table(name = "sale_inventory_details")
-@AllArgsConstructor
+@Table(name = "sale_item_details")
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class SaleInventoryDetailsEntity {
     @Id
@@ -17,15 +20,15 @@ public class SaleInventoryDetailsEntity {
 
     @ManyToOne
     @JoinColumn(name = "order_id", referencedColumnName = "order_id")
-    private SaleEntity saleEntity;
+    private SaleEntity sale;
 
     @ManyToOne
     @JoinColumn(name = "item_code", referencedColumnName = "item_code")
-    private InventoryEntity inventoryEntity;
-
-
+    private InventoryEntity inventory;
     private int size;
     private int item_qty;
-    private double selling_price;
+    private Double selling_price;
     private String status;
+    private String branch_code;
+    private Date date;
 }
