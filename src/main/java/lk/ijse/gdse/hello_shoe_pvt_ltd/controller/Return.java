b9e9927ea57e38b5a4031bf769454979a6105c85
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 18:35:31 +0530

    complete return controller

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Return.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Return.java
@@ -1,54 +1,54 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.controller;
 
-import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.InventoryDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.ReturnDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.service.ReturnService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/return")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class Return {
 
     private final ReturnService returnService;
-    @GetMapping("/health")
-    public String healthCheck(){
-        return "Hello I'm return controller and I'm okay!";
-    }
+
+
 
     @PostMapping
-    public boolean saveReturn(@RequestBody ReturnDTO returnDTO){
-        return returnService.saveReturn(returnDTO);
-
+    public boolean saveReturn(@RequestBody List<ReturnDTO> returnDTO) {
+        return returnService.add(returnDTO);
     }
 
-    @DeleteMapping
-    public boolean deleteReturn(@RequestParam String return_id){
-        return returnService.deleteReturn(return_id);
-    }
 
     @PutMapping
-    public boolean updateReturn(@RequestBody ReturnDTO returnDTO){
-        return returnService.updateReturn(returnDTO);
+    public boolean updateReturn(@RequestBody ReturnDTO returnDTO) {
+        return returnService.update(returnDTO);
     }
 
-    @GetMapping("/find")
-    public ReturnDTO searchReturn(@RequestParam String return_id){
-        System.out.println(return_id);
-        ReturnDTO returnDTO = new ReturnDTO();
-        returnDTO.setReturn_id(return_id);
-        return returnDTO;
 
+    @DeleteMapping
+    public boolean deleteReturn(@RequestParam String return_id) {
+        return returnService.delete(return_id);
     }
+
+
     @GetMapping
-    public ReturnDTO getAllReturns(){
-        ReturnDTO returnDTO = new ReturnDTO();
-        returnDTO.setReturn_id("R001");
-        return returnDTO;
+    public ReturnDTO searchReturn(@RequestParam String return_id) {
+        return returnService.search(return_id);
     }
 
 
+    @GetMapping("/all")
+    public List<ReturnDTO> getAllReturn() {
+        return returnService.getAll();
+    }
 
 
+    @GetMapping("health")
+    public String healthCheck() {
+        return "Hello I'm return Controller. I'm OK! Have a nice day!";
+    }
 }
