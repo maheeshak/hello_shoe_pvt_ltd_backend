e79dcdde746f7a66da6ee37b07e1004073d65429
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 19:04:35 +0530

    complete return service

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/ReturnService.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/ReturnService.java
@@ -4,11 +4,6 @@ import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.ReturnDTO;
 
 import java.util.List;
 
-public interface ReturnService {
-    boolean saveReturn(ReturnDTO returnDTO);
-    public boolean deleteReturn(String id);
-    public boolean updateReturn(ReturnDTO returnDTO);
-    public List<ReturnDTO> getAllReturns();
-
-    ReturnDTO getSelectedReturn(String id);
+public interface ReturnService extends SuperService<String, ReturnDTO>{
+    boolean add(List<ReturnDTO> returnDTO) ;
 }
