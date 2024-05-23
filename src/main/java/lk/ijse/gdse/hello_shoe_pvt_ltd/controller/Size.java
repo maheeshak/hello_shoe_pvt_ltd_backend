f675f6b454f4268bdad64ce530f00d324b670b39
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 18:35:41 +0530

    complete size controller

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Size.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/controller/Size.java
@@ -1,49 +1,52 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.controller;
 
-import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SaleDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SizeDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.service.SizeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/size")
 @RequiredArgsConstructor
-public class Size {
+@CrossOrigin(origins = "*")
+public class Size  {
+
     private final SizeService sizeService;
 
-    @GetMapping("/health")
-    public String healthCheck(){
-        return "Hello I'm size controller and I'm okay!";
-    }
-
     @PostMapping
-    public boolean saveSize(@RequestBody SizeDTO sizeDTO){
-        return sizeService.saveSize(sizeDTO);
+    public boolean saveSize(@RequestBody SizeDTO sizeDTO) {
+        return sizeService.add(sizeDTO);
     }
 
-    @DeleteMapping
-    public boolean deleteSize(@RequestParam String size_id){
-        return sizeService.deleteSize(size_id);
-    }
 
     @PutMapping
-    public boolean updateSize(@RequestBody SizeDTO sizeDTO){
-        return sizeService.updateSize(sizeDTO);
+    public boolean updateSize(@RequestBody SizeDTO sizeDTO) {
+        return sizeService.update(sizeDTO);
     }
 
-    @GetMapping("/find")
-    public SizeDTO searchSize(@RequestParam String size_id){
-        System.out.println(size_id);
-        SizeDTO sizeDTO = new SizeDTO();
-        sizeDTO.setSize_code(size_id);
-        return sizeDTO;
+
+    @DeleteMapping
+    public boolean deleteSize(@RequestParam String size_code) {
+        return sizeService.delete(size_code);
     }
+
 
     @GetMapping
-    public SizeDTO getAllSizes(){
-        SizeDTO sizeDTO = new SizeDTO();
-        sizeDTO.setSize_code("S001");
-        return sizeDTO;
+    public SizeDTO searchSize(@RequestParam String size_code) {
+        return sizeService.search(size_code);
+    }
+
+
+    @GetMapping("/all")
+    public List<SizeDTO> getAllSizes() {
+        return sizeService.getAll();
+    }
+
+
+    @GetMapping("/health")
+    public String healthCheck() {
+       return "Hello I'm Size Controller. I'm OK! Have a nice day!";
     }
 }
