aeb1b59870389bd465cb71081d32b121e6d2a28b
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 19:05:03 +0530

    complete size service

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/SizeService.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/SizeService.java
@@ -1,15 +1,6 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
 
-import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.CustomerDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SizeDTO;
 
-import java.util.List;
-
-public interface SizeService {
-    boolean saveSize(SizeDTO sizeDTO);
-    public boolean deleteSize(String id);
-    public boolean updateSize(SizeDTO sizeDTO);
-    public List<SizeDTO> getAllSizes();
-
-    SizeDTO getSelectedSize(String id);
+public interface SizeService extends SuperService<String, SizeDTO> {
 }
