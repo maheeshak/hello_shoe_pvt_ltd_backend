9d2052e03c58492f4bcfb71f05c67b64dfc1c2b9
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 19:04:50 +0530

    complete size inventory details service

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/SizeInventoryDetailsService.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/SizeInventoryDetailsService.java
@@ -2,7 +2,7 @@ package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
 
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SizeInventoryDetailsDTO;
 
-public interface SizeInventoryDetailsService  {
+public interface SizeInventoryDetailsService extends SuperService<Long, SizeInventoryDetailsDTO> {
 
   void updateQty(String item_code, String size_code,int qty);
 }
