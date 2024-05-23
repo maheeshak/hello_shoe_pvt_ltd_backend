187dbb45fdd6dff69c6e8d3b8ba4aecd7bc8040f
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 19:06:58 +0530

    add branch dto

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/dto/BranchDTO.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/dto/BranchDTO.java
@@ -1,23 +1,22 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.dto;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class BranchDTO implements SuperDTO{
     private String branch_code;
     private String branch_name;
     private String branch_manager;
-    private int no_of_employees;
-    private String contact;
     private String address;
-
+    private String contact;
+    private Integer no_of_employee;
+    @JsonManagedReference
     private List<EmployeeDTO> employees;
-
-
 }
