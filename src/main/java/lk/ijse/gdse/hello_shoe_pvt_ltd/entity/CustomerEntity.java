51e2c76d1a21f27ca53eda1a7da6980b21d10ab1
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 18:59:43 +0530

    complete customer entity

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/CustomerEntity.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/CustomerEntity.java
@@ -1,35 +1,33 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Gender;
-import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Level;
+import jakarta.persistence.*;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Gender;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Level;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
+
 @Entity
 @Table(name = "customer")
-public class CustomerEntity implements SuperEntity {
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CustomerEntity {
     @Id
     private String customer_code;
-    private String customer_name;
+    private String name;
     private Gender gender;
-    private Date joined_date;
+    private Date date;
     private Level level;
-    private int total_points;
+    private Integer point;
     private Date dob;
-    private String building_no;
+    private String building_number;
     private String lane;
     private String city;
     private String state;
@@ -37,8 +35,9 @@ public class CustomerEntity implements SuperEntity {
     private String contact;
     private String email;
     private Timestamp recent_purchase;
-    @OneToMany(mappedBy = "customerEntity")
-    private List<SaleEntity> sales =new ArrayList<>();;
-
+    private String active_state;
+    @OneToMany(mappedBy = "customer")
+    private List<SaleEntity> sales = new ArrayList<>();
+    private String customer_name;
 
 }
