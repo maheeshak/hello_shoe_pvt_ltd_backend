8851d8a401b0a48c90aef8b59c67e119b649da3c
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 19:03:53 +0530

    complete AuthenticationServiceIMPL

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/impl/AuthenticationServiceIMPL.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/impl/AuthenticationServiceIMPL.java
@@ -1,17 +1,17 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.service.impl;
-import lk.ijse.gdse.hello_shoe_pvt_ltd.dao.UserRepo;
+
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.UserDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.entity.UserEntity;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.repository.UserRepo;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.authObj.AuthenticationService;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.response.JwtAuthResponse;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.secure.SignIn;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.reqAndresp.secure.SignUp;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.service.JWTService;
-import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Mapping;
-import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Role;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Role;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.exeption.UserNotFoundException;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.map.Mapping;
 import lombok.RequiredArgsConstructor;
-
-
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -23,35 +23,25 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceIMPL implements AuthenticationService {
+
     private final UserRepo userRepo;
     private final JWTService jwtService;
     private final Mapping mapping;
 
     //utils
+
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
 
 
-
     @Override
-    public JwtAuthResponse signIn(SignIn signIn) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(signIn.getEmail(), signIn.getPassword()));
-                var email =userRepo.findByEmail(signIn.getEmail())
-                        .orElseThrow(() -> new UsernameNotFoundException("User not found")
-                );
-                var generateToken = jwtService.generateToken(email);
-                return JwtAuthResponse.builder()
-                        .token(generateToken)
-                        .build();
-
-
-
-
-
-
+    public JwtAuthResponse signIn(SignIn request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user = userRepo.findByEmail(request.getEmail())
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+        var jwt = jwtService.generateToken(user);
+        return JwtAuthResponse.builder().token(jwt).build();
     }
-
 
     @Override
     public JwtAuthResponse signUp(SignUp signIn) {
@@ -60,9 +50,10 @@ public class AuthenticationServiceIMPL implements AuthenticationService {
                 .email(signIn.getEmail())
                 .password(passwordEncoder.encode(signIn.getPassword()))
                 .role(Role.valueOf(signIn.getRole()))
+                .employee_code(signIn.getEmployee_code())
                 .build();
         var savedUser = userRepo.save(mapping.toUserEntity(buildUser));
-        var genToken =  jwtService.generateToken(savedUser);
+       var genToken =  jwtService.generateToken(savedUser);
         return JwtAuthResponse.builder().token(genToken).build();
     }
 
@@ -74,4 +65,6 @@ public class AuthenticationServiceIMPL implements AuthenticationService {
         jwtService.generateToken(user);
         return JwtAuthResponse.builder().token(jwtService.generateToken(user)).build().getToken();
     }
+
+
 }
