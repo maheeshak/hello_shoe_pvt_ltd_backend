abc38f943d89c0273f070d4e4827e4998444fd07
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 19:00:44 +0530

    complete supplier entity

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/SupplierEntity.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/entity/SupplierEntity.java
@@ -4,33 +4,34 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lk.ijse.gdse.hello_shoe_pvt_ltd.util.Category;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.util.enums.Category;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 @Entity
 @Table(name = "supplier")
-public class SupplierEntity implements SuperEntity{
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SupplierEntity {
     @Id
     private String supplier_code;
-    private String supplier_name;
+    private String name;
     private Category category;
-    private String building_no;
+    private String building_number;
     private String lane;
     private String city;
     private String state;
     private String postal_code;
     private String country;
-    private String contact_01;
-    private String contact_02;
+    private String mobile_contact;
+    private String landline_contact;
     private String email;
+    private String active_state;
 
-    @OneToMany(mappedBy = "supplierEntity")
+    @OneToMany(mappedBy = "supplier")
     private List<InventoryEntity> inventories;
 }
