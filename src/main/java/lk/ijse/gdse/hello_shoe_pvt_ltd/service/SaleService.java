589d809218cb1403dbe20d163f227448d116ec8c
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 19:05:09 +0530

    complete sale service

--- src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/SaleService.java
+++ src/main/java/lk/ijse/gdse/hello_shoe_pvt_ltd/service/SaleService.java
@@ -1,17 +1,24 @@
 package lk.ijse.gdse.hello_shoe_pvt_ltd.service;
 
-import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.InventoryDTO;
 import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SaleDTO;
-import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.extra.SaleDetailDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.SaleInventoryDetailsDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.extra.SaleDetailsDTO;
+import lk.ijse.gdse.hello_shoe_pvt_ltd.dto.extra.SaleInventoryDetailsTopDTO;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public interface SaleService {
-    public boolean saveSale(SaleDetailDTO saleDetailDTO);
-    public boolean deleteSale(String id);
-    public boolean updateSale(SaleDTO saleDTO);
-    public List<SaleDTO> getAllSales();
+public interface SaleService extends SuperService<String, SaleDTO>{
+    public boolean add(SaleDetailsDTO saleDetailsDTO);
 
-    SaleDTO getSelectedSale(String id);
+    String generateSaleID();
+
+    List<SaleInventoryDetailsDTO> searchSaleInventory(String orderId);
+
+    List<String> getTotalSalesBranches();
+
+    List<String> getTotalSalesBranchesThisMonth();
+
+    SaleInventoryDetailsTopDTO getTotalSalesInventoryThisMonth();
+
+    SaleInventoryDetailsTopDTO getTotalSalesInventoryThisYear();
 }
